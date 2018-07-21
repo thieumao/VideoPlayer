514a0e897e615411a5ee9747ac76a35356a4fc03
Thieu Mao <thieumao@gmail.com>
2018-07-21 19:29:05 +0700

    Add YoutubePlayer Lib

--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -7,17 +7,15 @@
 //
 
 import UIKit
+import YouTubePlayer
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var youtubePlayerView: YouTubePlayerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        youtubePlayerView.loadVideoID("xkhu732v3Wo")
     }
 
 
